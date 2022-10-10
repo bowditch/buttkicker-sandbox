f718e0645025543199f93b701ce66d3ff04dd702
John Bowditch <john.bowditch@me.com>
2022-10-10 10:28:42 -0400

    Project upload
    10, 12, 14, 16, 18, and 20Hz buttons to experiment with.

---
+++ Assets/Scripts/LFSample.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LFSample : MonoBehaviour
+{
+
+    private Renderer mouseColor; // All renderers inherit from Renderer. 
+    public AudioSource audioSource;
+    private void Awake()
+    {
+        mouseColor = GetComponent<Renderer>();
+        mouseColor.material.color = Color.white;
+    }
+
+    private void OnMouseEnter()
+    {
+        mouseColor.material.color = Color.blue;
+    }
+
+    private void OnMouseDown()
+    {
+        mouseColor.material.color = Color.green;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.enabled = true;
+        audioSource.Play();
+        Debug.Log("Audio is Playing");
+        return;
+
+    }
+
+    private void OnMouseExit()
+    {
+        mouseColor.material.color = Color.white;
+    }
+}
